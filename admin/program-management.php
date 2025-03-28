d9523d12f129b87689999e10198b021a27ccfb0a
payblis <info@payblis.com>
2025-03-28 13:34:23 +0100

    Update program-management.php

--- admin/program-management.php
+++ admin/program-management.php
@@ -3,9 +3,9 @@
 session_start();
 
 // Inclure les fichiers nécessaires
-require_once './config/database.php';
-require_once './includes/functions.php';
-require_once './includes/admin_functions.php';
+require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../includes/functions.php';
+require_once __DIR__ . '/../includes/admin_functions.php';
 
 // Vérifier si l'utilisateur est connecté
 if (!isLoggedIn()) {
