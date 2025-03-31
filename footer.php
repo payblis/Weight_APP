5f51eac799ba2b22d199de0e055bdfe348044318
payblis <info@payblis.com>
2025-03-31 18:37:49 +0200

    fix

---
+++ footer.php
@@ -0,0 +1,57 @@
+<footer class="bg-dark text-light py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 mb-4">
+                <h5>MyFity</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-light text-decoration-none">Compteur de calories</a></li>
+                    <li><a href="#" class="text-light text-decoration-none">Blog</a></li>
+                    <li><a href="#" class="text-light text-decoration-none">Premium</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 mb-4">
+                <h5>Communauté</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-light text-decoration-none">Forum</a></li>
+                    <li><a href="#" class="text-light text-decoration-none">Blog</a></li>
+                    <li><a href="#" class="text-light text-decoration-none">Guide communautaire</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 mb-4">
+                <h5>Légal</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-light text-decoration-none">Modalités</a></li>
+                    <li><a href="#" class="text-light text-decoration-none">Confidentialité</a></li>
+                    <li><a href="#" class="text-light text-decoration-none">Cookies</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 mb-4">
+                <h5>Contact</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-light text-decoration-none">Nous contacter</a></li>
+                    <li><a href="#" class="text-light text-decoration-none">API</a></li>
+                    <li><a href="#" class="text-light text-decoration-none">Emploi</a></li>
+                </ul>
+            </div>
+        </div>
+        <hr class="my-4">
+        <div class="row align-items-center">
+            <div class="col-md-6 text-center text-md-start">
+                <p class="mb-0">&copy; 2024 MyFity.com. Tous droits réservés.</p>
+            </div>
+            <div class="col-md-6 text-center text-md-end">
+                <div class="social-links">
+                    <a href="#" class="text-light me-3"><i class="fab fa-facebook"></i></a>
+                    <a href="#" class="text-light me-3"><i class="fab fa-twitter"></i></a>
+                    <a href="#" class="text-light me-3"><i class="fab fa-instagram"></i></a>
+                    <a href="#" class="text-light"><i class="fab fa-linkedin"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html> 
